21f28cd252945af533a505886e6c0dfea514f1f8
FelisitasMerry <felisitasmerry17@gmail.com>
2024-10-15 09:25:24 +0800

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class ThemeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Nama Kalian'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Headline Large',
+                style: Theme.of(context).textTheme.headlineLarge),
+            SizedBox(height: 20),
+            Text('Body Large', style: Theme.of(context).textTheme.bodyLarge),
+            Text('Body Medium', style: Theme.of(context).textTheme.bodyMedium),
+            Text('Body Small', style: Theme.of(context).textTheme.bodySmall),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Elevated Button'),
+            ),
+            SizedBox(height: 20),
+            OutlinedButton(
+              onPressed: () {},
+              child: Text('Outlined Button'),
+            ),
+            SizedBox(height: 20),
+            TextButton(
+              onPressed: () {},
+              child: Text('Text Button'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
